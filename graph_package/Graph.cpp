108f1e738589b1dfe8af0f981dea6e40fa211846
Mohamed Amin <mohamed.mos.amin@gmail.com>
2014-05-16 17:23:46 +0200

    graph done

--- graph_package/Graph.cpp
+++ graph_package/Graph.cpp
@@ -5,6 +5,7 @@
 
 
 
+
 Graph::Graph(){
     //Constructor
 }
@@ -21,13 +22,31 @@ CircleNode Graph::get_start_node(){
     return start_node;
 }
 
+void Graph::add_node(string name){
+    if(nodeMap.find(name) == nodeMap.end()){
+        CircleNode new_node = CircleNode();
+        nodeMap.insert(pair<string, CircleNode>(name, new_node));
+    }
+}
+
 void Graph::add_child(string  source , vector<string> destination)
 {
+    add_node(source);
+    SquareNode curNode;
+
+    for(int i = 0 ; i < destination.size() ; i++){
+        add_node(destination[i]);
+        curNode.add_circle_node(nodeMap[destination[i]]);
+    }
+
+    nodeMap[source].add_square_node(curNode);
+
+
 }
 
 vector<SquareNode> Graph::get_child(string  node_name)
 {
-
+    return nodeMap[node_name].get_childs();
 }
 
 
--- graph_package/SquareNode.cpp
+++ graph_package/SquareNode.cpp
@@ -6,6 +6,14 @@ SquareNode::SquareNode()
     //ctor
 }
 
+void SquareNode::add_circle_node(CircleNode new_node){
+    nodes.push_back(new_node);
+}
+
+vector<CircleNode> SquareNode::get_nodes(){
+    return nodes;
+}
+
 SquareNode::~SquareNode()
 {
     //dtor
